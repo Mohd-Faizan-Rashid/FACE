6f017fe79cbe5819b137095bb482abdfd24affa8
mohdfaizan <a-9739@kmclu.ac.in>
2025-01-25 23:50:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,)));
 
 // For any other routes, serve the index.html file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
